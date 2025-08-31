31161607cbf8a32f4c3c9295255d9b02f8166fc5
Igor de Paula <igoroliveira7896@gmail.com>
2025-08-31 15:39:37 -0300

    inclusão de templates e arquivos estáticos (css e img)

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog.views import ListaDePost, PostDetalhado
 
 urlpatterns = [
-    path('list/', ListaDePost.as_view(template_name='post_list.html')),
-    path('list/<slug:slug>/', PostDetalhado.as_view(template_name='base_post.html'), name = 'post-detalhado')
+    path('', ListaDePost.as_view(template_name='post_list.html')),
+    path('<slug:slug>/', PostDetalhado.as_view(template_name='post_detail.html'), name = 'post-detalhado')
 ]

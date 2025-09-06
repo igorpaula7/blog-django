8337f3664daedbd457b53a80f2de2fd00335a25a
Igor de Paula <igoroliveira7896@gmail.com>
2025-09-06 12:50:17 -0300

    adicionando o template post_detail.html e sua folha de estilo

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog.views import ListaDePost, PostDetalhado
 
 urlpatterns = [
-    path('', ListaDePost.as_view(template_name='post_list.html')),
+    path('', ListaDePost.as_view(template_name='post_list.html'), name = 'lista-de-posts'),
     path('<slug:slug>/', PostDetalhado.as_view(template_name='post_detail.html'), name = 'post-detalhado')
 ]
